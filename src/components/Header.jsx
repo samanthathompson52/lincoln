4602bd38127a0227ce3a752d534543aac5008739
Samantha Thompson <samanthathompson521@gmail.com>
2021-09-12 12:35:13 -0400

    Header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,32 +3,36 @@ import '../styles/Header.css';
 import {
   Link
 } from "react-router-dom";
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 const Header = () => {
 
   return (  
-    <div className = "header-background">
-      <nav class="navbar sticky-top navbar-expand-lg smoothscroll">
-        <Link class = "navbar-brand" to="/">Lincoln Pediatrics</Link>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar">
-            <Link class = "nav-item nav-link" to="/">Home</Link>
-            <Link class = "nav-item nav-link" to="/about">About Us</Link>
-            <Link class = "nav-item nav-link" to="/services">Services</Link>
-            <Link class = "nav-item nav-link" to="/education">Education</Link>
-            <Link class = "nav-item nav-link" to="/immunization">Immunization</Link>
-          </div>
-          <div>
-          <label class="switch">
-            <input id = "language-toggle" class="check-toggle check-toggle-round-flat " type="checkbox" />
-            <label for="language-toggle"></label>
-            <span class="on">En</span>
+  <div className = "header-background">
+    <Navbar expand="xl">
+      <Container fluid>
+        <Navbar.Brand><Link className="navbar-brand" to="/">Lincoln Pediatrics</Link></Navbar.Brand>
+        <Navbar.Toggle aria-controls="lincoln-header" className="ml-auto"/>
+          <Navbar.Collapse id="lincoln-header">
+            <Nav className="ml-auto">
+              <Link class = "nav-item" to="/">Home</Link>
+              <Link class = "nav-item" to="/about">About Us</Link>
+              <Link class = "nav-item" to="/services">Services</Link>
+              <Link class = "nav-item" to="/education">Education</Link>
+              <Link class = "nav-item" to="/immunization">Immunization</Link>
+            </Nav>
+            <div className="pt-3">
+            <label class="switch">
+              <input id = "language-toggle" class="check-toggle check-toggle-round-flat " type="checkbox" />
+              <label for="language-toggle"></label>
+              <span class="on">En</span>
             <span class="off">ES</span>
-          </label>
+            </label>
           </div>
-        </div>    
-      </nav>
-    </div>
+          </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  </div>
   );
 }
 
